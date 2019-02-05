b62d0c3c23bf0a41d8efbfaf3e1d20a4d025c664
sanjayameex <36154196+sanjayameex@users.noreply.github.com>
2019-02-05 20:39:23 +0530

    sanjayameex

---
+++ begining.php
@@ -0,0 +1,12 @@
+<?php
+$n="hello";
+$n1="welcome";
+echo $n1;
+echo $n;
+ <h6> welcome to php</h6>
+   <h6> welcome to php</h6>
+   <h7> welcome to php</h7>
+   <h7> welcome to php</h7>
+   <h8> welcome to php</h8>
+  <h5>contibuted by sanjay.ameex</h5>
+?> 
